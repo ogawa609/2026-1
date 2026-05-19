e4424f90ddbf1046cf2d552a1be9989e64894da4
Ogawa <eduardovitorogawa@gmail.com>
2026-05-19 16:52:34 -0300

    Alocacao_Dinamica/Geral/Aloc_02

---
+++ 06_alocacao_dinamica/01_geral/aloc_02/Resultados/edu/main/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "utils.h"
+#include <stdlib.h>
+
+
+int main()
+{
+    int linhas,colunas;
+    scanf("%d %d",&linhas,&colunas);
+
+    int **matriz = CriaMatriz(linhas,colunas);
+
+    LeMatriz(matriz,linhas,colunas);
+    ImprimeMatrizTransposta(matriz,linhas,colunas);
+    LiberaMatriz(matriz,linhas);
+
+    return 0;
+}
