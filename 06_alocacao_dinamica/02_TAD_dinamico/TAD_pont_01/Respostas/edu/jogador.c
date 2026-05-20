0ffffe187d77e92db7095e8627c763e5ffd28f37
Edu <eduardovitorogawa@gmail.com>
2026-05-20 16:47:26 -0300

    Tad_Alocacao/Tad_01

---
+++ 06_alocacao_dinamica/02_TAD_dinamico/TAD_pont_01/Respostas/edu/jogador.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "jogador.h"
+
+
+/**
+ * Aloca e retorna uma estrutura do tipo tJogador.
+ * Se a alocação falhar, o programa é encerrado.
+ *
+ *  @param idJogador o ID do jogador (1 ou 2).
+ * 
+ * @return a estrutura do tipo tJogador alocada.
+ */
+tJogador* CriaJogador(int idJogador)
+{
+    tJogador* jog = (tJogador*) malloc(sizeof(tJogador));
+
+    jog->id = idJogador;
+
+    return jog;
+}
+
+
+/**
+ * Libera a memória de uma estrutura do tipo tJogador.
+ * 
+ * @param jogador a estrutura do tipo tJogador a ser liberada.
+ */
+void DestroiJogador(tJogador* jogador)
+{
+    free(jogador);
+}
+
+
+/**
+ * Lê uma jogada e armazena em uma estrutura do tipo tJogada.
+ * 
+ * @param jogador o jogador atual.
+ * @param tabuleiro o tabuleiro atual.
+ */
+void JogaJogador(tJogador* jogador, tTabuleiro* tabuleiro)
+{
+    
+}
+
+
+/**
+ * Verifica se o jogador venceu o jogo.
+ * 
+ * @param jogador o jogador atual.
+ * @param tabuleiro o tabuleiro atual.
+ * 
+ * @return 1 se o jogador venceu, 0 caso contrário.
+ */
+int VenceuJogador(tJogador* jogador, tTabuleiro* tabuleiro);
