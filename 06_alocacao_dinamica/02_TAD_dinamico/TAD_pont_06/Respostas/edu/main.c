fc757de77fff1cc0bf43fa14c7d77fd8ec814270
Ogawa <eduardovitorogawa@gmail.com>
2026-06-04 12:17:46 -0300

    tad_aloc_06

---
+++ 06_alocacao_dinamica/02_TAD_dinamico/TAD_pont_06/Respostas/edu/main.c
@@ -0,0 +1,112 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "matrix_utils.h"
+
+
+int main()
+{
+    int x1,y1,x2,y2;
+    scanf("%d %d",&x1,&y1);
+
+    tMatrix* m1 = MatrixCreate(x1,y1);
+    MatrixRead(m1);
+
+    scanf("%d %d",&x2,&y2);
+    tMatrix*  m2 = MatrixCreate(x2,y2);
+    MatrixRead(m2);
+
+    while(1)
+    {
+        printf("1 - Somar matrizes\n");
+        printf("2 - Subtrair matrizes\n");
+        printf("3 - Multiplicar matrizes\n");
+        printf("4 - Multiplicacao de uma matriz por escalar\n");
+        printf("5 - Transposta de uma matriz\n");
+        printf("6 - Encerrar o programa\n");
+        printf("Opcao escolhida:\n");
+
+        int op;
+        scanf("%d",&op);
+
+        if(op==1)
+        {
+            if(PossibleMatrixSum(m1,m2))
+            {
+                tMatrix* m3 = MatrixAdd(m1,m2);
+                MatrixPrint(m3);
+                MatrixFree(m3);
+            }
+            else
+            {
+                printf("Erro: as dimensoes da matriz nao correspondem\n\n");
+            }
+        }
+        else if(op==2)
+        {
+            if(PossibleMatrixSub(m1,m2))
+            {
+            
+                tMatrix* m3 = MatrixSub(m1,m2);
+                MatrixPrint(m3);
+                MatrixFree(m3);
+            }
+            else
+            {
+                printf("Erro: as dimensoes da matriz nao correspondem\n\n");
+            }
+        }
+        else if(op==3)
+        {
+            if(PossibleMatrixMultiply(m1,m2))
+            {
+                tMatrix* m3 = MatrixMultiply(m1,m2);
+                MatrixPrint(m3);
+                MatrixFree(m3);
+            }
+            else
+            {
+                printf("Erro: o numero de colunas da primeira matriz eh diferente do numero de linhas da segunda matriz\n\n");
+            }
+        }
+        else if(op==4)
+        {
+            int scalar,matriz;
+            scanf("%d %d",&scalar,&matriz);
+
+            if(matriz==1)
+            {
+                tMatrix* m3 = MatrixMultiplyByScalar(m1,scalar);
+                tMatrix* temp = m1;
+                m1 = m3;
+                MatrixPrint(m3);
+                MatrixFree(temp);
+            }
+            else if(matriz==2)
+            {
+                tMatrix* m3 = MatrixMultiplyByScalar(m2,scalar);
+                tMatrix* temp = m2;
+                m2 = m3;
+                MatrixPrint(m3);
+                MatrixFree(temp);
+            }
+        }
+        else if(op==5)
+        {
+            tMatrix* t1 = TransposeMatrix(m1);
+            MatrixPrint(t1);
+            tMatrix* t2 = TransposeMatrix(m2);
+            MatrixPrint(t2);
+            MatrixFree(t1);
+            MatrixFree(t2);
+        }
+        else if(op==6)
+        {
+            break;
+        }
+    }
+
+    MatrixFree(m1);
+    MatrixFree(m2);
+
+    return 0;
+}
